22927dd330bc46a97655d086bf2fca45de21d918
Shariful Islam <sharifulmamun7@gmail.com>
2021-11-11 02:54:23 +0600

    products api added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ip6hg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -20,7 +20,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    console.log("connected with the server");
+    const productsCollection = client.db("bicycleBarn").collection("products");
+    //*GET API or GET Home page product
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      // const products = await cursor.toArray();
+      const products = await cursor.limit(3).toArray();
+      res.send(products);
+    });
+    //*GET API or GET all product
+    app.get("/allProducts", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const products = await cursor.toArray();
+      // const products = await cursor.limit(3).toArray();
+      res.send(products);
+    });
   } finally {
     // await client.close();
   }
---
+++ readme.md
@@ -0,0 +1,18 @@
+#Project Name : Bicycle Barn (Server side)
+
+This site live link: https://bicycle-barn.web.app/
+
+### Description
+
+On this website, a user can log in via using google sign. If user valid authenticate person he/she manage booking and a user see their(my) booking.
+Data save on mongodb server.
+
+About the Project:
+
+- User Login system
+- Add service
+- Manage Service (delete ,update)
+- user Authentication
+- Mongodb use as server
+- Redirect system
+- Logout system
